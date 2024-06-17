ca96461dc1562af671da6db3d1d6d8370007b0a7
Moshik Alper <moshikalper@gmail.com>
2024-06-17 12:25:41 +0300

    Create bookshop.service and controller and create getBook func to return it

--- js/bookshop.controller.js
+++ js/bookshop.controller.js
@@ -1,35 +1,12 @@
 'use strict'
 
-var gBooks = [
-    {
-        id: 'bg4J48',
-        title: 'Slaughterhouse-Five',
-        price: 173,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-
-    {
-        id: 'cd4J38',
-        title: 'Catch-22',
-        price: 120,
-        imgUrl: 'lori-ipsi.jpg'
-    },
-
-    {
-        id: 'fr4228',
-        title: 'The Sirens of Titan',
-        price: 200,
-        imgUrl: 'lori-ipsi.jpg'
-    }
-]
-
 function onInit() {
     renderBookList()
 }
 
 function renderBookList() {
     const elBookList = document.querySelector('.book-list')
-    const strHtml = gBooks.map(book => `
+    const strHtml = getBooks().map(book => `
            <tr>
                 <th scope="row">${book.title}</th>
                 <td>${book.price}</td>
---
+++ js/services/bookshop.service.js
@@ -0,0 +1,28 @@
+'use strict'
+
+var gBooks = [
+    {
+        id: 'bg4J48',
+        title: 'Slaughterhouse-Five',
+        price: 173,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+
+    {
+        id: 'cd4J38',
+        title: 'Catch-22',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+
+    {
+        id: 'fr4228',
+        title: 'The Sirens of Titan',
+        price: 200,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+]
+
+function getBooks() {
+    return gBooks
+}
