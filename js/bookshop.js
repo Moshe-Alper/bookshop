c8cfec91d6b69df41c3d4f460197bd818ba38dc1
Moshik Alper <moshikalper@gmail.com>
2024-06-17 11:12:43 +0300

    Init repo

---
+++ js/bookshop.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function onReadBook(bookId) {
+    console.log(bookId)
+}
+
+function onUpdateBook(bookId) {
+    console.log(bookId)
+}
+
+function onDeleteBook(bookId) {
+    console.log(bookId)
+}
