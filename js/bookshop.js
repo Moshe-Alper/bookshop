d0c41347999dbbeae70d42885d5019c43465569c
Moshik Alper <moshikalper@gmail.com>
2024-06-17 12:13:43 +0300

    Add dynamic rendering

--- js/bookshop.js
+++ js/bookshop.js
@@ -1,5 +1,48 @@
 'use strict'
 
+var gBooks = [
+    {
+        id: 'bg4J48',
+        title: 'Slaughterhouse-Five',
+        price: 173,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+
+    {
+        id: 'cd4J38',
+        title: 'Catch-22',
+        price: 120,
+        imgUrl: 'lori-ipsi.jpg'
+    },
+
+    {
+        id: 'fr4228',
+        title: 'The Sirens of Titan',
+        price: 200,
+        imgUrl: 'lori-ipsi.jpg'
+    }
+]
+
+function onInit() {
+    renderBookList()
+}
+
+function renderBookList() {
+    const elBookList = document.querySelector('.book-list')
+    const strHtml = gBooks.map(book => `
+           <tr>
+                <th scope="row">${book.title}</th>
+                <td>${book.price}</td>
+                <td>
+                    <button onclick="onReadBook('${book.id}')" class="read">Read</button>
+                    <button onclick="onUpdateBook('${book.id}')" class="update">Update</button>
+                    <button onclick="onDeleteBook('${book.id}')" class="delete">Delete</button>
+                </td>
+            </tr>
+        `)
+    elBookList.innerHTML = strHtml.join('')
+}
+
 function onReadBook(bookId) {
     console.log(bookId)
 }
